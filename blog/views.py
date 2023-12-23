20776d33c36cfc35f2bb305c6d45ecf9f9f7eef3
kk <kkforever66@gmail.com>
2023-12-23 14:22:07 +0800

    added forms and post_edit module

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 # The dot before models means current directory or current application.
 # Both views.py and models.py are in the same directory. This means we can use . and the name of the file (without .py).
 # Then we import the name of the model (Post).
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -14,3 +16,30 @@ def post_list(request):
 def post_detail(request,pk):
     post=get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
+
+def post_new(request):
+    if request.method=="POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
